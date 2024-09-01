5c12d2dbbc614b0d3d23ec1ddf4080905f2f6e3b
Sifflet_blanc <76435000+Sifflet-Blanc@users.noreply.github.com>
2024-09-01 14:39:04 +0900

    finalisation de la preparation intelliJ IDEA

--- Pb024/java/src/Main.java
+++ Pb024/java/src/Main.java
@@ -1,31 +1,31 @@
 public class Main {
     private final int[] tab;
 
-    public static int factoriel(int n){
+    public static int factoriel(int n) {
         int rep = 1;
-        for(int i = 2; i <= n; i++){
-            rep = rep*i;
+        for (int i = 2; i <= n; i++) {
+            rep = rep * i;
         }
         return rep;
     }
 
-    public Main(int[] tab){
+    public Main(int[] tab) {
         this.tab = tab;
     }
 
-    private int getTab(int i){
+    private int getTab(int i) {
         return this.tab[i];
     }
 
-    private void setTab(int i, int valeur){
+    private void setTab(int i, int valeur) {
         this.tab[i] = valeur;
     }
 
-    private int length(){
+    private int length() {
         return this.tab.length;
     }
 
-    private void swap(int i, int j){
+    private void swap(int i, int j) {
         int tmp = getTab(i);
         setTab(i, getTab(j));
         setTab(j, tmp);
@@ -34,28 +34,28 @@ public class Main {
     /**
      * @param j j > i
      */
-    private void sorted_swap(int i, int j){
-        for(int k = j; k > i; k--){
-            swap(k,k-1);
+    private void sorted_swap(int i, int j) {
+        for (int k = j; k > i; k--) {
+            swap(k, k - 1);
         }
     }
 
-    private void i_eme_permutation_aux(int flag, int i){
-        if(flag<length()-1) {
-            int nb_poss = factoriel(length() - flag)/(length() - flag);
+    private void i_eme_permutation_aux(int flag, int i) {
+        if (flag < length() - 1) {
+            int nb_poss = factoriel(length() - flag) / (length() - flag);
             sorted_swap(flag, i / nb_poss + flag);
             i_eme_permutation_aux(flag + 1, i - nb_poss * (i / nb_poss));
         }
     }
 
-    private void i_eme_permutation(int i){
-        i_eme_permutation_aux(0, i-1);
+    private void i_eme_permutation(int i) {
+        i_eme_permutation_aux(0, i - 1);
     }
 
 
-    public String to_String(){
+    public String to_String() {
         String rep = "";
-        for(int i = 0; i < length(); i++){
+        for (int i = 0; i < length(); i++) {
             rep += getTab(i);
         }
         return rep;
@@ -64,7 +64,7 @@ public class Main {
     public static void main(String[] args) {
         int n = 10;
         int[] tab = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             tab[i] = i;
         }
         Main test = new Main(tab);
