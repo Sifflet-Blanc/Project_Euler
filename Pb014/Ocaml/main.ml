a0d7a227b130994e430d3966bca90e03ac194ad6
Sifflet_blanc <76435000+Sifflet-Blanc@users.noreply.github.com>
2024-09-13 13:38:41 +0900

    Résolution problème 14

--- Pb014/Ocaml/main.ml
+++ Pb014/Ocaml/main.ml
@@ -1,29 +1,34 @@
+module IntMap = Map.Make(Int)
+
 let suivant_collatz n : int =
     if n mod 2 == 0 then 
         n/2
     else 3*n+1
 
 (*tab contien en i la longeur de la sequence collazt commencant par i*)
-let rec length_of_collatz_sequence_of_number n tab: unit = 
-    if Option.is_none (Map.find_opt n tab) then begin 
-        let suiv_n = suivant_collatz n in
-        let tmp = length_of_collatz_sequence_of_number suiv_n in
-        match tmp with
-        | Some x -> Map.add n (x+1) tab; Some x+1
-        | None -> Printf.printf "Impossible"
-    end
+let rec length_of_collatz_sequence_of_number n tab: int = 
+    let opt = IntMap.find_opt n !tab in
+    match opt with
+    | Some x -> x
+    | None -> (let x = length_of_collatz_sequence_of_number (suivant_collatz n) tab in
+                tab := IntMap.add n (x+1) !tab;
+                (x+1))
+
 
 let longest_collatz_sequence =
-    let tab = Map.empty in
-    Map.add 1 1 tab;
+    let tab = ref IntMap.empty in
+    tab := IntMap.add 1 1 !tab;
     let max = ref 1 in
-    for i = 1 to 13 do
+    let max_key = ref 1 in
+    for i = 1 to 999999 do
         let tmp = length_of_collatz_sequence_of_number i tab in
-        match tmp with 
-        | Some t -> if t > !max then max := t
-        | None -> ()
+        (*Printf.printf "%d %d\n" i tmp;*)
+        if tmp > !max then begin
+            max := tmp;
+            max_key := i
+        end
     done;
-    !max
+    !max_key
 
 let _ = 
     Printf.printf "%d\n" (longest_collatz_sequence)
