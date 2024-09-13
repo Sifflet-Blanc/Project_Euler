2df94acf6126ffd85abc78d05a420d5501285bed
Sifflet_blanc <76435000+Sifflet-Blanc@users.noreply.github.com>
2024-09-13 17:13:07 +0900

    Résolution problème 15

--- Pb015/Ocaml/main.ml
+++ Pb015/Ocaml/main.ml
@@ -1,19 +1,21 @@
-let chemin_possible hauteur dist_arrivee =
-    if Int.abs hauteur == dist_arrivee then
-        [-hauteur/hauteur]
-    else [-1; 1]
-
 (*dist_arrivee = cote*cote pour le probleme décrit (pour un carre dont l'arrivée 
 est en bas a droite et le depart en haut a gauche*)
 let compte_chemin_possible dist_arrivee = 
-    let rec aux hauteur dist_arrivee = 
-        if dist_arrivee == 1 then 1
-        else appel (chemin_possible hauteur dist_arrivee) hauteur dist_arrivee 0
-    and appel l hauteur dist_arrivee acc = 
-        match l with 
-        | [] -> acc
-        | t::q -> appel q hauteur dist_arrivee (acc + (aux (hauteur + t) (dist_arrivee-1))) in
+    let rec aux hauteur dist_arrivee : int = 
+        if hauteur < 0 then 0
+        else begin 
+            if dist_arrivee == 1 || Int.abs hauteur == dist_arrivee then 1
+            else begin 
+                let a = aux (hauteur + 1) (dist_arrivee-1) in
+                let b = aux (hauteur - 1) (dist_arrivee-1) in
+                if hauteur == 0 then 
+                    (a + b)*2
+                else
+                    a + b
+            end
+        end in
     aux 0 dist_arrivee
 
 let _ = 
-    Printf.printf "%d\n" (compte_chemin_possible (20*20))
+    let n = 20 in
+    Printf.printf "%d\n" (compte_chemin_possible (2*n))
